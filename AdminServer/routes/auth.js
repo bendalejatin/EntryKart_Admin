52524efe03afd0a78ffa582adda85fa419c1209b
bendalejatin <bendalejatin35@gmail.com>
2025-04-25 01:40:22 +0530

    updated code

--- AdminServer/routes/auth.js
+++ AdminServer/routes/auth.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
